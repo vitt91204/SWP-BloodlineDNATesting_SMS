dab070ec34e13374da11cb28bce83b809a6c7346
vitt91204 <somethingthaticantforget@gmail.com>
2025-06-27 14:52:58 +0700

    fix payment

--- BE/DNAServicesSystem/Services/PaymentDTO/UpdateStatusRequest.cs
+++ BE/DNAServicesSystem/Services/PaymentDTO/UpdateStatusRequest.cs
@@ -8,7 +8,6 @@ namespace Services.PaymentDTO
 {
     public class UpdateStatusRequest
     {
-        public int PaymentId { get; set; }
         public string Status { get; set; } // e.g., "Pending", "Completed", "Failed"
         public DateTime PaidAt { get; set; } = DateTime.UtcNow;
     }
